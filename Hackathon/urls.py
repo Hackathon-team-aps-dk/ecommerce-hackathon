ad83f5586aa577020d01586b9d9065e50ec01836
Arnav Bajaj <arnavbajaj9@gmail.com>
2021-09-15 00:59:32 +0530

    Debugged 7

--- Hackathon/urls.py
+++ Hackathon/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from Register import views as v
+from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
