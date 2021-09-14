8729162b50f5411e211a1219d07b9a30a0f80c6f
Arnav Bajaj <arnavbajaj9@gmail.com>
2021-09-15 00:02:14 +0530

    Debug

--- Hackathon/settings.py
+++ Hackathon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_en!iecm_@vlc4e3@3x!ptjitm7f54j7(!e05rqaaq_y#scs%5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0' , 'apsdk-hackathon.herokuapp.com']
 
