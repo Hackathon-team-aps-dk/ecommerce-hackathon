568c3ca390395b2a9ba5724c40e049cf87bfec56
Arnav Bajaj <arnavbajaj9@gmail.com>
2021-09-15 00:57:35 +0530

    Debugged 6

--- Hackathon/settings.py
+++ Hackathon/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Main.apps.MainConfig',
-    'Register.apps.RegisterConfig',
+    'register.apps.RegisterConfig',
     'crispy_forms'
 ]
 
