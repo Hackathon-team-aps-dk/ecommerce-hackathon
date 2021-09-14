a822cc14843d87da80d0b8429f74485e13902249
Arnav Bajaj <arnavbajaj9@gmail.com>
2021-09-15 01:34:32 +0530

    Debugged 16

--- Hackathon/urls.py
+++ Hackathon/urls.py
@@ -19,7 +19,8 @@ from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/' , v.register , name = "register"),
+    path('' , include("register.urls")),
+    #path('register/' , v.register , name = "register"),
     path('', include("Main.urls")),
     path('' , include("django.contrib.auth.urls")),
 ]
