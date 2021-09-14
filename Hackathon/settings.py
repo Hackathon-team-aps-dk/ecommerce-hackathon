d9a867c72944e1758092975362ea0826bfa1b132
Arnav Bajaj <arnavbajaj9@gmail.com>
2021-09-14 23:30:14 +0530

    Config

--- Hackathon/settings.py
+++ Hackathon/settings.py
@@ -19,13 +19,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_en!iecm_@vlc4e3@3x!ptjitm7f54j7(!e05rqaaq_y#scs%5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1' , 'apsdk-hackathon.herokuapp.com']
 
 
 # Application definition
@@ -131,7 +143,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
 LOGIN_REDIRECT_URL = "/home"
-
-DEBUG = False
-
-ALLOWED_HOSTS = ['127.0.0.1' , 'apsdk-hackathon.herokuapp.com']
