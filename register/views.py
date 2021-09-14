1fb82408d02167c2a61632b8d31bc2341af3c0e3
Arnav Bajaj <arnavbajaj9@gmail.com>
2021-09-15 00:31:41 +0530

    Registaration

--- register/views.py
+++ register/views.py
@@ -10,4 +10,4 @@ def register(response):
             return redirect('/home')
     else:
         form = RegisterForm()
-    return render(response , 'register/register.html' , {"form" : form})
+    return render(response , 'Templates/register/register.html' , {"form" : form})
