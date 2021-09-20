433003deed2b11dcd6836873c2fc36c6c6234ec5
Arnav Bajaj <arnavbajaj9@gmail.com>
2021-09-20 14:20:15 +0530

    cart update

--- Product/templatetags/custom_filters.py
+++ Product/templatetags/custom_filters.py
@@ -2,7 +2,7 @@ from django import template
 from django.shortcuts import render
 from django.template import Template
 from django.contrib.auth.models import User
-from Product.models import Product
+from Product.models import Product , Cart
 
 register = template.Library()
 
@@ -14,5 +14,12 @@ def idToProduct(id):
     product = Product.objects.get(id=id)
     return product.id
 
+def add(carts):
+    net = 0
+    for cart in carts:
+        net += cart.cost
+    return net
+
 register.filter('idToUser', idToUser)
 register.filter('idToProduct', idToProduct)
+register.filter('add', add)
