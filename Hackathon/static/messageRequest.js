d1903ae57a535f20bf193e1f3f5bbd71a4cf9d13
Arnav Bajaj <arnavbajaj9@gmail.com>
2021-09-21 13:01:20 +0530

    Fix additional

--- Hackathon/static/messageRequest.js
+++ Hackathon/static/messageRequest.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
                 $("#msg-area").empty();
                 for (var key in response.messages)
                 {
+                    //let textToShow = "<div class='container'><b>"+response.messages[key].sender+"</b><p>"+response.messages[key].messageText+"</p></div>"
                     let textToShow = "<li> " + response.messages[key].sender +  " to "  + response.messages[key].reciever +" : " + response.messages[key].messageText+" </li><br>"
-                    console.log(textToShow)
                     $("#msg-area").append(textToShow);
                 }
             },
